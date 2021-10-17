f9251b8b6a7a34c149a215ed2cf240a24037e216
Hendrik-rohmawan <hendrikrohmawan@gmail.com>
2021-10-17 21:06:25 +0700

    flappy bird

---
+++ Assets/pipaScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipaScript : MonoBehaviour {
+	float nilaiRandom;
+	// Use this for initialization
+	void Start () {
+		nilaiRandom = Random.Range (-6.03f, -1.66f);
+		transform.position = new Vector2 (transform.position.x, nilaiRandom);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector2.MoveTowards (transform.position, Vector2.left * 1000, Time.deltaTime * 5);
+	}
+}
---
+++ Assets/playerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class playerMovement : MonoBehaviour {
+	Rigidbody2D Rb;
+	public float jumpForce;
+	float score;
+
+	public Text scoreTxt;
+	public GameObject replay;
+	public GameObject gameover;
+
+	void Start () {
+		Time.timeScale = 1;
+		Rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreTxt.text = "Score : " + score;
+
+		if (Input.GetMouseButtonDown (0)) {
+			Rb.velocity = Vector2.up * jumpForce;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision){
+		if(collision.gameObject.CompareTag("point")) {
+			score++;
+		}
+		if (collision.gameObject.CompareTag("pipa") ||
+			collision.gameObject.CompareTag("ground")) {
+			
+			Time.timeScale = 0;
+			replay.SetActive (true);
+			gameover.SetActive (true);
+
+		}
+	}
+
+	public void ReplayGame()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+	}
+}
